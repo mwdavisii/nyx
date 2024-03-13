0236f19d3cf1b7c45e9bd98a01e1fb734cc48862
Mike D. <mwdavisii@gmail.com>
2024-03-13 17:50:31 -0500

    fixed hostname in wsl script

--- setup/wsl/step3.sh
+++ setup/wsl/step3.sh
@@ -1,6 +1,6 @@
 #!/run/current-system/sw/bin/bash
 
 cd ../../
-sudo nixos-rebuild switch --flake .#wsl
+sudo nixos-rebuild switch --flake .
 systemctl --user enable auto-fix-vscode-server.service
 ln -sfT /run/current-system/etc/systemd/user/auto-fix-vscode-server.service ~/.config/systemd/user/auto-fix-vscode-server.service
