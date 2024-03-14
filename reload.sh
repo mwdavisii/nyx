89849c411537cad5d6d06a30e8940f7879a75abf
Mike D. <mwdavisii@gmail.com>
2024-03-14 15:37:50 -0500

    Added nix-on-droid to reload.sh

--- reload.sh
+++ reload.sh
@@ -2,9 +2,12 @@
 
 osName=$(uname -s)
 hostName=$(uname -n)
+userName=$(whoami)
 
 if [[ $osName == "Darwin" ]]; then
   darwin-rebuild switch --flake .
+elif [[ $userName == "nix-on-droid"]] them
+  nix-on-droid switch --flake .
 else
   sudo nixos-rebuild switch --flake .
   pkill gpg-agent #force any changes to gpg
