0b2c76a4228e32ef4fca11e99d99274f3abc6ca6
Mike D. <mwdavisii@gmail.com>
2024-03-14 15:34:00 -0500

    removed rnix-lsp

--- home/shared/default.nix
+++ home/shared/default.nix
@@ -4,9 +4,6 @@
 
   nixpkgs = {
     config = {
-      permittedInsecurePackages = [
-          "nix-2.15.3"
-      ];
       allowUnfree = true;
       allowBroken = true;
       allowUnsupportedSystem = true;
