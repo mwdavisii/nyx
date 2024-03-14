3794ffb4d1ea6f835e4d7a395f94f36e0dd3659f
Mike D. <mwdavisii@gmail.com>
2024-03-13 22:38:40 -0500

    wip

--- home/droid/home.nix
+++ home/droid/home.nix
@@ -25,20 +25,6 @@
       neovim
       marksman
       nodejs
-      nodePackages.bash-language-server
-      nodePackages.pyright
-      nodePackages.typescript-language-server
-      nodePackages.vim-language-server
-      nodePackages.write-good
-      rnix-lsp
-      stylua
-      shfmt
-      nixpkgs-fmt
-      prettierd
-      proselint
-      nodePackages.eslint_d
-      nodePackages.yaml-language-server
-
     ];
     #files
   };
@@ -46,7 +32,43 @@
     home.file.".config/zsh".source = ../config/.config/zsh;
     home.file.".config/nvim".source = ../config/.config/nvim;
     home.file.".config/starship/starship.toml".source = ../config/.config/starship/starship.toml;
-    
+    xdg.dataFile = with pkgs.tree-sitter-grammars; 
+    let
+      grammars = with pkgs.tree-sitter-grammars; [
+        tree-sitter-bash
+        tree-sitter-c
+        tree-sitter-c-sharp
+        tree-sitter-comment
+        tree-sitter-cpp
+        tree-sitter-css
+        tree-sitter-go
+        tree-sitter-javascript
+        tree-sitter-json
+        tree-sitter-lua
+        tree-sitter-make
+        # Currently this does not point to the correct markdown parser
+        # Correct one is: https://github.com/MDeiml/tree-sitter-markdown
+        # tree-sitter-markdown
+        tree-sitter-nix
+        tree-sitter-regex
+        tree-sitter-rust
+        tree-sitter-toml
+        tree-sitter-tsx
+        tree-sitter-typescript
+        tree-sitter-vim
+        tree-sitter-yaml
+      ];
+      parseName = x: removeSuffix "-grammar" (removePrefix "tree-sitter-" (getName x));
+      # parsers = listToAttrs (map
+      #   (p: {
+      #     name = "nvim/parser/${parseName p}.so";
+      #     value = { source = "${p}/parser"; };
+      #   })
+      #   grammars);
+      parsers = { };
+      in parsers // {
+      "nvim/lib/libsqlite3.so".source = "${pkgs.sqlite.out}/lib/libsqlite3.so";
+    };
 /*
   nyx.modules = {
     dev = {
