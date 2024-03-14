ea057d746389df93392bd98dd68d6ef9ae4fcbe4
Mike D. <mwdavisii@gmail.com>
2024-03-13 21:52:03 -0500

    wip

--- system/droid/hosts/nix-on-droid/default.nix
+++ system/droid/hosts/nix-on-droid/default.nix
@@ -3,10 +3,32 @@
 {
   imports = [./system.nix];
   
-  nyx = {
-    modules = {
-      user.home = ../../../../home/droid/home.nix;
-    };
+  home = {      
+    stateVersion = "23.11";
+    packages = with pkgs; [
+      rustup
+      vhs
+      bat
+      bash
+      coreutils-full
+      moreutils
+      ripgrep
+      gnupg
+      fd
+      sd
+      dua
+      vhs
+      just
+      comma
+      nix-index
+      tuxmux
+      zsh
+      kubectl
+      fluxcd
+      wget
+      vim
+      git
+    ];
   };
   
 }
