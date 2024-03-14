228d930eb5d4f6915025d0382b222b72d2914942
Mike D. <mwdavisii@gmail.com>
2024-03-13 21:20:17 -0500

    wip

---
+++ system/droid/hosts/default/system.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, userConf, inputs, hostname, ... }: 
+with pkgs;
+with userConf;
+{
+  system.stateVersion = "23.11";
+  #programs.zsh.enable = true;
+  #environment.pathsToLink = ["/share/zsh/"];
+  #environment.shells = [pkgs.zsh];
+  #security.sudo.wheelNeedsPassword = false;
+
+  # Turn on flag for proprietary software
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+
+    registry = {
+      nixpkgs = {
+        flake = inputs.nixpkgs;
+      };
+    };
+  };
+}
