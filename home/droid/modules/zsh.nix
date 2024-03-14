72d9a38a2d351acec8c56c516a36c22375cbe5da
Mike D. <mwdavisii@gmail.com>
2024-03-14 14:48:34 -0500

    droid wip

--- home/droid/modules/zsh.nix
+++ home/droid/modules/zsh.nix
@@ -2,7 +2,10 @@
 
 {  
       home.packages = with pkgs;
-        [ zsh ] ++ nix-zsh-completions;
+        [ zsh
+          nix-zsh-completions
+        ];
+        
       home.file.".zshenv".source = ../../../config/.zshenv;
       xdg.configFile."zsh".source = ../../../config/.config/zsh;
 }
