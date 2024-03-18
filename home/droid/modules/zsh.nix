8e66429d8864e9adfbf69c1744b7f7a6394caec6
Mike D. <mwdavisii@gmail.com>
2024-03-17 19:49:10 -0500

    working on droid

--- home/droid/modules/zsh.nix
+++ home/droid/modules/zsh.nix
@@ -1,18 +1,18 @@
 { config, lib, pkgs, ... }:
 let 
-    pluginsDir = xdg.cacheHome."zsh/plugins";
+    pluginsDir = ".zsh/plugins";
 in
 
 {  
       home.packages = with pkgs;
         [ 
-          zsh
+          zsh                                   
           nix-zsh-completions
         ];
 
       home.file.".zshenv".source = ../../config/.zshenv;
-      xdg.configFile."zsh".source = ../../config/.config/zsh;
-      xdg.dataFile."zsh/nyx_zshrc".text = ''
+      home.configFile."zsh".source = ../../config/.config/zsh;
+      home."zsh/nyx_zshrc".text = ''
         "source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
         "source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"}
        '';
