89dd501eab5037b5d74f514d2a9bd3310348c576
Mike D. <mwdavisii@gmail.com>
2024-03-27 11:15:44 -0500

    build test

--- system/nixos/hosts/worklt/system.nix
+++ system/nixos/hosts/worklt/system.nix
@@ -52,7 +52,7 @@
     LC_TIME = "en_US.UTF-8";
   };
   environment.variables = {
-    NIXOS_OZONE_WL = "1";
+    #NIXOS_OZONE_WL = "1";
     PATH = [
       "\${HOME}/.local/bin"
       "\${HOME}/.config/rofi/scripts"
