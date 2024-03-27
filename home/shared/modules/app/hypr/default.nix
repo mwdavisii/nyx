cf9147538e3c1087a234d0e5f769f89de310aa42
Mike D. <mwdavisii@gmail.com>
2024-03-27 13:02:46 -0500

    Good for Now

--- home/shared/modules/app/hypr/default.nix
+++ home/shared/modules/app/hypr/default.nix
@@ -1,5 +1,4 @@
 { config, lib, pkgs, ... }:
-
 with lib;
 let
   cfg = config.nyx.modules.app.hypr;
@@ -12,18 +11,20 @@ in
   options.nyx.modules.app.hypr = {
     enable = mkEnableOption "hypr configuration";
   };
-  config = mkIf cfg.enable {
 
+  config = mkIf cfg.enable {
     home.packages = with pkgs; [
       waybar
       swww
     ];
-
+    xdg.configFile."hypr".source = ../../../config/.config/hyper;
     #test later systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
     wayland.windowManager.hyprland = {
       enable = true;
       systemd.enable = true;
-      xdg.configFile."hypr".source = ../../../config/.config/hyper;
-    }
-      };
-  }
+    };
+
+  };
+}
+  
+  
