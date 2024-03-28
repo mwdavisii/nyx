578b7ac8beb1fdf4eb5d4aaa093458625a8a6306
Mike D. <mwdavisii@gmail.com>
2024-03-28 05:22:22 -0500

    updated GPU info

---
+++ system/nixos/hosts/hephaestus/default.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./system.nix ];
+  nyx = {
+    modules = {
+      user.home = ./home.nix;
+    };
+
+    secrets = {
+      awsSSHKeys.enable = true;
+      awsConfig.enable = true;
+      userSSHKeys.enable = true;
+      userPGPKeys.enable = true;
+    };
+
+    profiles = {
+      desktop = {
+        enable = true;
+      };
+    };
+  };
+}
