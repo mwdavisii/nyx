cf737598aae77aa4923a4864fea1b74ba398ab3c
Mike D. <mwdavisii@gmail.com>
2024-03-28 14:46:51 -0500

    removed libl

---
+++ home/nixos/themes/default.nix
@@ -0,0 +1,17 @@
+gtk = {
+      enable = true;
+      iconTheme = {
+        name = "Yaru-magenta-dark";
+        package = pkgs.yaru-theme;
+      };
+
+      theme = {
+        name = "Tokyonight-Dark-B-LB";
+        package = pkgs.tokyo-night-gtk;
+      };
+
+      cursorTheme = {
+        name = "Bibata-Modern-Classic";
+        package = pkgs.bibata-cursors;
+      };
+    };
