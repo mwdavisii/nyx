ac423e8c96b4e83ced8f964d4cbaf4fc9b61a1a4
Mike D. <mwdavisii@gmail.com>
2024-04-09 16:10:28 -0500

    Fixed hephaestus disk-config and deleted work WSL profile

--- system/nixos/hosts/hephaestus/disk-config.nix
+++ system/nixos/hosts/hephaestus/disk-config.nix
@@ -1,16 +1,18 @@
 { config, lib, pkgs, modulesPath, inputs, ... }:
 {
-fileSystems."/" =
-    { device = "/dev/disk/by-uuid/155193bb-865d-4295-8aee-728af878243e";
+  fileSystems."/" ={ 
+      device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
-    };
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2275-59D6";
+  fileSystems."/boot" =  { 
+      device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
-    };
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/e00f254f-af03-4efa-af3b-a8931734960a"; }
-    ];
+  swapDevices =[ 
+    { 
+      device = "/dev/disk/by-label/swap"; 
+    }
+  ];
 }
