075a80e74e390c04b43e57d73bcf3b7cdb309ad3
Mike D. <mwdavisii@gmail.com>
2024-04-15 08:48:53 -0500

    fixed hammerspoon lua mod key

--- home/config/.hammerspoon/init.lua
+++ home/config/.hammerspoon/init.lua
@@ -1,14 +1,11 @@
 -- Define the modifier key
-local super = {"ctrl", "alt", "cmd"}
-local superShift = {"ctrl", "alt", "cmd", "lshift"}
-
 -- hs.loadSpoon("SpoonInstall")
 -- #spoon.SpoonInstall.use_syncinstall = true
 -- spoon.SpoonInstall:andUse("AClock")
 -- spoon.SpoonInstall:andUse("ReloadConfiguration")
 -- spoon.ReloadConfiguration:start()
 
-require("aliases")
+require("commands")
 require("apps")
 require("hswindow")
 
