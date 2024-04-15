6e7dae27c3ba12e0eb2ff6684764122b7a9919bc
Mike D. <mwdavisii@gmail.com>
2024-04-15 11:01:19 -0500

    prtscreen (F13) to screenshot

--- home/config/.hammerspoon/commands.lua
+++ home/config/.hammerspoon/commands.lua
@@ -13,3 +13,4 @@ hs.hotkey.bind({"ctrl"}, "z", nil, function() hs.eventtap.keyStroke({"cmd"}, "z"
 hs.hotkey.bind({"ctrl"}, "f", nil, function() hs.eventtap.keyStroke({"cmd"}, "f") end)
 hs.hotkey.bind({"ctrl"}, "s", nil, function() hs.eventtap.keyStroke({"cmd"}, "s") end)
 hs.hotkey.bind({"ctrl"}, "return", nil, function() hs.eventtap.keyStroke({"cmd"}, "return") end)
+hs.hotkey.bind(nil, "f13", nil, function() hs.eventtap.keyStroke({"ctrl, cmd, shift"}, "4") end)
