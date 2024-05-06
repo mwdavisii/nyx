c6ce744260beb96f1c1a0eb3fee81c4c3c0c81c6
Mike Davis <mwdavisii@gmail.com>
2024-05-06 12:36:22 -0500

    Windows Tiling v1

--- setup/windows/install-apps.ps1
+++ setup/windows/install-apps.ps1
@@ -4,6 +4,9 @@
 #import shared scripts
 Import-Module ..\shared\common.psm1
 
+# enable long paths
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+
 Install-ChocoPackage git
 Install-ChocoPackage sublimetext4
 Install-ChocoPackage win32yank
@@ -11,5 +14,17 @@ Install-ChocoPackage vscode
 Install-ChocoPackage discord
 Install-ChocoPackage docker-desktop
 Install-ChocoPackage winrar
+Install-ChocoPackage nerdfont-hack
+Install-ChocoPackage alacritty
+# K8s Tooling
+Install-ChocoPackage kubernetes-cli
+Install-ChocoPackage flux-cd
+# Shell
+winget install starship
 
+# komorebi & wkhd
+winget install LGUG2Z.komorebi
+winget install LGUG2Z.whkd
 
+# Install PS Modules
+Install-Module VirtualDesktop
