289ea61d1e7b903274181ab7d8c7f52cea08a7e8
Mike Davis <mwdavisii@gmail.com>
2024-05-06 16:04:46 -0500

    pywal and winwal working

--- setup/windows/3_configure-settings.ps1
+++ setup/windows/3_configure-settings.ps1
@@ -4,8 +4,9 @@
 Copy-Item -Recurse -Force ../../home/config/.config ~/
 
 # specific items
-Copy-Item ../../home/config/.config/PowerShell/Microsoft.PowerShell_profile.ps1 $HOME\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
-Copy-Item ../../home/config/.config/PowerShell/Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+Copy-Item -Force ../../home/config/.config/PowerShell/Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+Copy-Item ../../home/config/.config/PowerShell/Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
 Copy-Item -Recurse -Force ../../home/config/.config/alacritty/ $Env:AppData\alacritty
 Copy-Item ../../home/config/.config/komorebic/komorebi.json ~/komorebi.json
 
