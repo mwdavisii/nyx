7af8ea6288e80698fade8bbd62d408985e68854c
Mike Davis <mwdavisii@gmail.com>
2024-05-06 12:36:22 -0500

    Windows Tiling v1

---
+++ setup/windows/kmonad.ps1
@@ -0,0 +1,12 @@
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Invoke-WebRequest -useb get.scoop.sh | iex
+scoop install stack
+
+# clone the KMonad repository (assuming you have `git` installed)
+Set-Location $HOME\Downloads
+git clone https://github.com/kmonad/kmonad.git
+Set-Location $HOME\Downloads\kmonad
+# compile KMonad (this will first download GHC and msys2, it takes a while)
+stack build
+# install kmonad.exe (copies kmonad.exe to %APPDATA%\local\bin\)
+stack install
