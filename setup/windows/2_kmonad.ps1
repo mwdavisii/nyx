a2d684077e08f14795c0804dd2890bc60184954a
Mike Davis <mwdavisii@gmail.com>
2024-05-06 17:52:24 -0500

    changed forks to use ~/.source

--- setup/windows/2_kmonad.ps1
+++ setup/windows/2_kmonad.ps1
@@ -1,12 +1,12 @@
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 scoop install stack
 
+Copy-Item ..\..\home\config\.config\kmonad\windows\kmonad.cmd "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 # clone the KMonad repository (assuming you have `git` installed)
-Set-Location $HOME\Downloads
-git clone https://github.com/kmonad/kmonad.git
-Set-Location $HOME\Downloads\kmonad
+Remove-Item -Recurse -Force $env:USERPROFILE\.source\kmonad
+git clone https://github.com/kmonad/kmonad.git $env:USERPROFILE\.source\kmonad
+Set-Location $env:USERPROFILE\.source\kmonad
 # compile KMonad (this will first download GHC and msys2, it takes a while)
 stack build
 # install kmonad.exe (copies kmonad.exe to %APPDATA%\local\bin\)
 stack install
-Copy-Item ..\..\home\config\.config\kmonad\windows\kmonad.cmd "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
