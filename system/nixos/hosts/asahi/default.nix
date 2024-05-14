716c6ac0584cf630ed1402b343eec8d548955bfa
Mike D. <mwdavisii@gmail.com>
2024-05-14 08:28:04 -0500

    Bootable

---
+++ system/nixos/hosts/asahi/default.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./system.nix ];
+  nyx = {
+    modules = {
+      user.home = ./home.nix;
+      system = {
+        bluetooth.enable = true;
+        garbagecollection.enable = true;
+        hyprlogin.enable = true;
+        openvpn.enable = true;
+        openrgb.enable = true;
+        centraltimezone.enable = true;
+        yubilogin.enable = true;
+        hyprland.enable = true;
+        kmonad.enable = true;
+      };
+    };
+
+    secrets = {
+      awsSSHKeys.enable = true;
+      awsConfig.enable = true;
+      userSSHKeys.enable = true;
+      userPGPKeys.enable = true;
+      workvpn.enable = true;
+    };
+
+    profiles = {
+      desktop = {
+        enable = true;
+      };
+    };
+  };
+}
---
+++ system/nixos/hosts/asahi/home.nix
@@ -0,0 +1,104 @@
+{ config, pkgs, ... }:
+
+{
+  # Manage home-manager with home-manager (inception)
+  programs.home-manager.enable = true;
+  # Install man output for any Nix packages.
+  programs.man.enable = true;
+  manual.manpages.enable = true;
+  programs.zsh.enable = true;
+
+  home = {
+    sessionVariables = {
+      PATH = "$PATH:~/.local/bin:~/.config/rofi/scripts";
+    };
+    stateVersion = "23.11";
+  };
+
+  nyx = {
+    modules = {
+      gaming = {
+        bsdgames.enable = true;
+        lutris.enable = false;
+        mahjong.enable = true;
+        minesweeper.enable = true;
+        retroarch.enable = false;
+        steam.enable = false;
+      };
+      desktop = {
+        hypr.enable = true;
+        rofi.enable = true;
+        gtk.enable = true;
+        wlogout.enable = true;
+        cava.enable = true;
+        kmonad.enable = true;
+      };
+      app = {
+        alacritty = {
+          enable = true;
+        };
+        mysql-workbench.enable = true;
+        kitty.enable = true;
+        firefox.enable = true;
+        jupyter.enable = true;
+        obs.enable = true;
+
+        scrcpy.enable = true;
+        qemu.enable = true;
+        wezterm = {
+          enable = false; #having issue with wezterm and wayland
+          package = null;
+          fontSize = 14;
+        };
+        vscode.enable = true;
+      };
+      dev = {
+        androidSDK.enable = true;
+        cc.enable = true;
+        rust.enable = true;
+        go.enable = true;
+        dhall.enable = true;
+        lua.enable = true;
+        nix.enable = true;
+        node.enable = true;
+        python.enable = false; #conflict w/ Jupyter
+      };
+      shell = {
+        awscliv2.enable = true;
+        bash.enable = true;
+        bat.enable = true;
+        btop.enable = true;
+        direnv.enable = true;
+        eza.enable = true;
+        fzf.enable = true;
+        gcp.enable = true;
+        git = {
+          enable = true;
+          signing.signByDefault = false;
+        };
+        gnupg = {
+          enable = true;
+          enableService = true;
+          enableSSHSupport = true;
+        };
+        jq.enable = true;
+        k8sTooling.enable = true;
+        lf.enable = true;
+        lorri.enable = false;
+        mcfly.enable = true;
+        neofetch.enable = true;
+        neovim.enable = true;
+        networking.enable = true;
+        ranger.enable = false;
+        starship.enable = true;
+        terraform.enable = true;
+        tmux.enable = true;
+        usbutils.enable = true;
+        wal.enable = true;
+        xdg.enable = true;
+        zellij.enable = true;
+        zsh.enable = true;
+      };
+    };
+  };
+}
