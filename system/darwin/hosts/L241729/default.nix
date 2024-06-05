68434130f9612babb94daf8f8a182753fb1b95ff
Mike D. <mwdavisii@gmail.com>
2024-06-05 16:14:37 -0500

    New Host

---
+++ system/darwin/hosts/L241729/default.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [./system.nix]; 
+  nyx = {
+    modules = {
+      user.home = ./home.nix;
+      system.yabai.enable = true;
+    };
+
+    secrets = {
+      awsSSHKeys.enable = false;
+      awsConfig.enable = true;
+      userSSHKeys.enable = true;
+      userPGPKeys.enable = true;
+    };
+
+    profiles = {
+      macbook.enable = true;
+    };
+  };
+}
