7961652b5669fe8cc9feb17aee2d3d62f265c138
Mike D. <mwdavisii@gmail.com>
2024-08-26 10:02:48 -0500

    some azure and flake updates

--- home/shared/modules/shell/azurecli/default.nix
+++ home/shared/modules/shell/azurecli/default.nix
@@ -2,8 +2,12 @@
 
 with lib;
 let 
+  #inherit (lib) mkIf isDerivation;
+  #inherit (builtins) filter attrValues;
+  #azure-cli = pkgs.azure-cli.withExtensions (filter (item: isDerivation item) (attrValues pkgs.azure-cli-extensions) (attrValues != pkgs.azure-cli-extensions.blockchain));
   cfgHome = config.xdg.configHome;
   cfg = config.nyx.modules.shell.azurecli;
+  
 in
 {
   options.nyx.modules.shell.azurecli = {
@@ -11,7 +15,13 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs;[ azure-cli ];
-    
+    home.packages = with pkgs; [
+      (azure-cli.withExtensions [ 
+        azure-cli.extensions.aks-preview 
+        azure-cli-extensions.k8s-extension
+        #azure-cli-extensions.k8s-configuration
+        #azure-cli.extensions.k8s-configuration 
+      ])
+    ];
   };
 }
