5d24a53540982b57d09d8776911e12a990ccf9e3
Mike D. <mwdavisii@gmail.com>
2024-08-26 08:38:54 -0500

    Updates

--- users/mdavis67.nix
+++ users/mdavis67.nix
@@ -3,7 +3,8 @@
   email = "mwdavisii@gmail.com";
   displayName = "Mike D.";
   signingKey = "0x93150D93ECA375FA";
-  hashedPassword = "$6$HaIadJwel0Euc.oL$R6fcjiA/DACT1E1Vd2Hijask0cAigVLKpBcF0ShkHVfdvkeRujTM6HY7yxGGQri9Qjx.koeasIzhOCotTvfLT.";
+  #setting the pwd has caused some issue w/ jamf.
+  #hashedPassword = "$6$HaIadJwel0Euc.oL$R6fcjiA/DACT1E1Vd2Hijask0cAigVLKpBcF0ShkHVfdvkeRujTM6HY7yxGGQri9Qjx.koeasIzhOCotTvfLT.";
   yubiKeySerials = [ "11821015" "10180701" "8689918" "13125330" ];
   windowsUserDirName = "";
 }
