750b93d6d03b816646a2d26eff8defbe033cca2a
Mike D. <mwdavisii@gmail.com>
2025-01-03 21:31:57 -0600

    WSL good. working on host

--- system/nixos/hosts/L242731/disk-config.nix
+++ system/nixos/hosts/L242731/disk-config.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, modulesPath, inputs, ... }:
 {
+    fileSystems."/" =
+    { device = "/dev/disk/by-uuid/2fd4f325-19f7-423d-b823-ae444c115022";
+      fsType = "ext4";
+    };
+    
     fileSystems."/boot" =
       { 
         device = "/dev/disk/by-uuid/CF98-8911";
@@ -12,3 +17,5 @@
       { device = "/dev/disk/by-uuid/107dd2cc-0ed8-4bbd-9546-05a13d69b41b"; }
     ];
 }
+
+
