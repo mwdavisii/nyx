4a03e5a3deb393b903946c770e5513829c025299
Mike D. <mwdavisii@gmail.com>
2025-01-16 12:47:25 -0600

    nvim, opentofu, azurecli

---
+++ home/config/.config/nvim/nvim/lua/eden/core/filetype.lua
@@ -0,0 +1,12 @@
+vim.filetype.add({
+  filename = {
+    ["justfile"] = "just",
+    ["Justfile"] = "just",
+  },
+  extension = {
+    ["kdl"] = "kdl",
+    ["dhall"] = "dhall",
+    ["tl"] = "teal",
+  },
+  -- pattern = {},
+})
