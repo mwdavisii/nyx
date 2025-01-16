003544d366e12563f2ca785cfb43f88d536f86e4
Mike D. <mwdavisii@gmail.com>
2025-01-16 15:13:52 -0600

    added docker to nixos

---
+++ system/nixos/modules/system/docker/default.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, modulesPath, hostName, ... }:
+with lib;
+let
+  cfg = config.nyx.modules.system.docker;
+in
+{
+  options.nyx.modules.system.docker = {
+    enable = mkEnableOption "openvpn Settings";
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+    services.hardware.openrgb.enable = true;
+    environment.systemPackages = with pkgs; [
+      docker
+    ];
+  };
+}
