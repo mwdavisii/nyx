0d8e4df18658b796b09682e96dc8cb9fba3923b3
Mike D. <mwdavisii@gmail.com>
2025-03-11 14:09:12 -0500

    A few fixes and a flake update.

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     kmonad = {
-      url = "git+https://github.com/kmonad/kmonad?submodules=1&dir=nix";
+      url = "github:kmonad/kmonad?dir=nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Secrets
--- switch.sh
+++ switch.sh
@@ -3,6 +3,10 @@ hostName=$(uname -n)
 userName=$(whoami)
 
 if [[ $osName == "Darwin" ]]; then
+  if [ -f /usr/local/bin/warp-cli ]; then
+    # disable because nixos.org certs aren't trusted.
+    warp-cli disconnect
+  fi
   darwin-rebuild --show-trace switch --flake .
 elif [[ $userName == "nix-on-droid" ]]; then
   nix-on-droid switch --show-trace --flake .
