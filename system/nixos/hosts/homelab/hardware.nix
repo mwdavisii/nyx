c25fc8f19781611490c1ce77cec0a8b3b1d31cab
Mike D. <mwdavisii@gmail.com>
2025-08-26 20:11:54 -0500

    updated flake

--- system/nixos/hosts/homelab/hardware.nix
+++ system/nixos/hosts/homelab/hardware.nix
@@ -6,9 +6,12 @@
         (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "" ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 }
 
