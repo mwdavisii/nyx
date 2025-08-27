199db331a6cff287386b7701ceef53af971c6ea2
Mike D. <mwdavisii@gmail.com>
2025-08-26 20:07:55 -0500

    updated flake

--- system/nixos/hosts/homelab/system.nix
+++ system/nixos/hosts/homelab/system.nix
@@ -13,34 +13,7 @@
     time.hardwareClockInLocalTime = true;
     # Configure keymap in X11
     services = {
-      libinput.enable = false;
-      xserver = {
-        enable = false;
-        videoDrivers = [ "amdgpu" ];
-        xkb = {
-          layout = "us";
-          variant = "";
-        };
-      };
-      printing.enable = false;
       openssh.enable = true;
-      gvfs.enable = false; # Mount, trash, and other functionalities
       tumbler.enable = true;
-      pipewire = {
-        enable = false;
-        alsa.enable = false;
-        alsa.support32Bit = false;
-        pulse.enable =false;
-      };
-    };
-
-    # Enable sound with pipewire.
-    hardware.pulseaudio.enable = false;Q
-
-    xdg.portal = {
-      enable = false;
-      wlr.enable = false;
-      config.common.default = "*";
-    };
   };
 }
