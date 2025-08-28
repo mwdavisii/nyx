c80ead3d4e07dd0c3fbbcd6c484ee9ae88e94af5
Mike D. <mwdavisii@gmail.com>
2025-08-27 19:27:44 -0500

    Added file system import

--- system/nixos/hosts/homelab/hardware.nix
+++ system/nixos/hosts/homelab/hardware.nix
@@ -4,6 +4,7 @@
     imports =
     [ 
         (modulesPath + "/profiles/qemu-guest.nix")
+        (./disk-config.nix)
     ];
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
@@ -14,4 +15,3 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 }
-
