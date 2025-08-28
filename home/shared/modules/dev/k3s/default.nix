01849837cc928a7a5de560da38b28b9cc4dcd72b
Mike D. <mwdavisii@gmail.com>
2025-08-27 19:52:26 -0500

    adding k3s

--- home/shared/modules/dev/k3s/default.nix
+++ home/shared/modules/dev/k3s/default.nix
@@ -4,13 +4,15 @@ with lib;
 let cfg = config.nyx.modules.dev.k3s;
 in
 {
-
+  options.nyx.modules.dev.k3s = {
+        enable = mkEnableOption "K3s Installation";
+    };
   config = mkIf cfg.enable {
     home = {
       packages = with pkgs; [
         k3s
       ];
-        };
+    };
   };
 }
 
