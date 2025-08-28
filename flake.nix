823560eb1b97f30689991e4e4780682956330df7
Mike D. <mwdavisii@gmail.com>
2025-08-27 19:42:18 -0500

    Added file system import

--- flake.nix
+++ flake.nix
@@ -32,11 +32,9 @@
     # Secrets
     agenix.url = "github:ryantm/agenix";
     secrets = {
-      url = "git+ssh://git@github.com/mwdavisii/nix-secrets.git";
+      url = "git+ssh://git@github.com/mwdavisii/nix-secrets/ffd9cdc8962a2f3f294903e582f014c1326fe3e8";
       flake = false;
-      allRefs = true;
     };
-
     # MacOS
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     darwin = {
