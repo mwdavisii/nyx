a1381d9ab5dc4dcd6d4e7709d2fcf9682a0528a7
Mike D. <mwdavisii@gmail.com>
2025-09-05 16:23:48 -0500

    adding HA vnet to k3s

--- system/nixos/hosts/k3s/system.nix
+++ system/nixos/hosts/k3s/system.nix
@@ -8,45 +8,62 @@
 
     ];
   config = {
-    networking.networkmanager.enable = lib.mkForce true;
-    networking.useDHCP = lib.mkDefault true;
-    time.hardwareClockInLocalTime = true;
+   { config, pkgs, ... }:
 
-    #vlan configu
-
-    ## HA
-    networking.interfaces.ens20.useDHCP = false;
-    networking.interfaces.ens20.ipv4.addresses = [{
-      address = "10.40.50.200";
-      prefixLength = 24;
-    }];
-    ## HA
-    #networking.interfaces.ens19.useDHCP = false;
-    #networking.interfaces.ens19.ipv4.addresses = [{
-    #  address = "10.40.250.200";
-    #  prefixLength = 24;
-    #}];
-    networking.defaultGateway = "10.40.250.1";
+{
+    #### Interfaces ####
+    # Trunk: carry VLANs, no L3 config on the host for now
+    networking.interfaces.ens18.useDHCP = false;
 
 
-    # Configure keymap in X11
+    # Infra (VLAN 250)
+    networking.interfaces.ens19 = {
+      useDHCP = false;
+      ipv4.addresses = [{ address = "10.40.250.21"; prefixLength = 24; }];
+    };
+
+    # Home Automation (VLAN 50)
+    networking.interfaces.ens20 = {
+      useDHCP = false;
+      ipv4.addresses = [{ address = "10.40.50.21"; prefixLength = 24; }];
+    };
+
+    #### Routing tables ####
+    networking.routingTables = {
+      infra250 = 250;
+      ha50     = 150;
+    };
+
+    #### Routes in per-interface tables (no defaults in main) ####
+    networking.interfaces.ens19.ipv4.routes = [
+      { address = "10.40.250.0"; prefixLength = 24; table = "infra250"; }
+      { address = "0.0.0.0";     prefixLength = 0;  via = "10.40.250.1"; table = "infra250"; }
+    ];
+    networking.interfaces.ens20.ipv4.routes = [
+      { address = "10.40.50.0"; prefixLength = 24; table = "ha50"; }
+      { address = "0.0.0.0";    prefixLength = 0;  via = "10.40.50.1"; table = "ha50"; }
+    ];
+
+    #### Policy rules: pick table by source IP ####
+    networking.routingRules = [
+      { family = "ipv4"; from = "10.40.250.21/32"; table = "infra250"; priority = 1000; }
+      { family = "ipv4"; from = "10.40.50.21/32";  table = "ha50";     priority = 1001; }
+    ];
+
+    #### rp_filter: loose mode for multi-homed hosts ####
+    boot.kernel.sysctl = {
+      "net.ipv4.conf.all.rp_filter" = "2";
+      "net.ipv4.conf.default.rp_filter" = "2";
+    };
+
+    #### DNS (system-wide) ####
+    # Keep DNS on your preferred segment (often infra):
+    networking.nameservers = [ "192.168.0.2" "10.40.250.2"];
+  
     services = {
       openssh.enable = true;
       qemuGuest.enable = true;
  
     };
   };
-}
-
-
-{
-  networking.vlans.eth0_70 = {
-    interface = "eth0";
-    id = 70;
-  };
-  networking.interfaces.eth0_70.ipv4.addresses = [{
-    address = "10.40.70.21";
-    prefixLength = 24;
-  }];
-  # Default route likely stays on your mgmt interface; omit here unless you want it.
 }
