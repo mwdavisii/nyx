bf6c5c9a5cd18556ff9747cedf7f234577413f8f
Mike D. <mwdavisii@gmail.com>
2025-09-05 12:59:14 -0500

    adding HA vnet to k3s

--- system/nixos/hosts/k3s/system.nix
+++ system/nixos/hosts/k3s/system.nix
@@ -11,6 +11,24 @@
     networking.networkmanager.enable = lib.mkForce true;
     networking.useDHCP = lib.mkDefault true;
     time.hardwareClockInLocalTime = true;
+
+    #vlan configu
+
+    ## HA
+    networking.interfaces.ens20.useDHCP = false;
+    networking.interfaces.ens20.ipv4.addresses = [{
+      address = "10.40.50.200";
+      prefixLength = 24;
+    }];
+    ## HA
+    #networking.interfaces.ens19.useDHCP = false;
+    #networking.interfaces.ens19.ipv4.addresses = [{
+    #  address = "10.40.250.200";
+    #  prefixLength = 24;
+    #}];
+    networking.defaultGateway = "10.40.250.1";
+
+
     # Configure keymap in X11
     services = {
       openssh.enable = true;
@@ -18,4 +36,17 @@
  
     };
   };
+}
+
+
+{
+  networking.vlans.eth0_70 = {
+    interface = "eth0";
+    id = 70;
+  };
+  networking.interfaces.eth0_70.ipv4.addresses = [{
+    address = "10.40.70.21";
+    prefixLength = 24;
+  }];
+  # Default route likely stays on your mgmt interface; omit here unless you want it.
 }
