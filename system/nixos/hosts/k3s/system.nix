c27f4ffe1a5d8ae17e51675128dfc298a4947f39
Mike D. <mwdavisii@gmail.com>
2025-09-06 13:16:21 -0500

    network fix

--- system/nixos/hosts/k3s/system.nix
+++ system/nixos/hosts/k3s/system.nix
@@ -33,7 +33,7 @@
       }
     ];
     networking.defaultGateway = "10.40.250.1";
-    networking.nameservers = ["10.40.250.2" "192.168.0.2"];
+    networking.nameservers = ["192.168.0.2" "10.40.250.2"];
     time.hardwareClockInLocalTime = true;
     # Configure keymap in X11
     services = {
