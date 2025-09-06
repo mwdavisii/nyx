5c6f832188ebc96799b3a19ca8203c8323d96f0e
Mike D. <mwdavisii@gmail.com>
2025-09-06 12:48:42 -0500

    adding HA vnet to k3s

--- system/nixos/hosts/k3s/system.nix
+++ system/nixos/hosts/k3s/system.nix
@@ -8,8 +8,43 @@
 
     ];
   config = {
-    networking.networkmanager.enable = lib.mkForce true;
-    networking.useDHCP = lib.mkDefault true;
+    networking.networkmanager.enable = lib.mkForce false;
+    networking.useDHCP = lib.mkDefault false;
+
+    #host
+    networking.interfaces.eth0.eth18.addresses = [
+    {
+      address = "10.40.250.201";
+      prefixLength = 24;
+    }
+    ];
+    #IoT vlan
+    networking.interfaces.eth0.eth19.addresses = [
+      {
+        address = "10.40.40.40";
+        prefixLength = 24;
+      }
+    ];
+    # Security VLAN
+    networking.interfaces.eth0.eth20.addresses = [
+      {
+        address = "10.40.70.40";
+        prefixLength = 24;
+      }
+    ];
+    networking.defaultGateway = "10.40.250.1";
+
+    networking.extraRoutes = [
+      {
+        dest = "10.40.70.0/24";
+        via = "10.40.70.1";
+      }
+      {
+        dest = "10.40.40.0/24";
+        via = "10.40.40.1";
+      }
+    ];
+
     time.hardwareClockInLocalTime = true;
     # Configure keymap in X11
     services = {
@@ -19,3 +54,5 @@
     };
   };
 }
+
+
