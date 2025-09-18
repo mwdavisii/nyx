5c82fdf8ae4d431a160d6bc9b13ebf56a7c320d2
Mike D. <mwdavisii@gmail.com>
2025-09-18 09:06:04 -0500

    removed old aws keys from wsl2

--- flake.nix
+++ flake.nix
@@ -57,6 +57,10 @@
     };
 
     # WSL
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     #Droid
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid/release-23.11";
