65747ecb2aadbe021548f21bdbc537f75c2d00fc
Mike D. <66962344+mwdavisii@users.noreply.github.com>
2025-09-18 14:26:01 -0500

    Wsl (#71)
    * python upgrade
    
    * cleaned up some hosts and updated readme
    
    * removed old aws keys from wsl2
    
    * fixed zsh
    
    * made pywall work with wsl

--- home/shared/modules/shell/wal/default.nix
+++ home/shared/modules/shell/wal/default.nix
@@ -3,6 +3,16 @@
 with lib;
 let
   cfg = config.nyx.modules.shell.wal;
+    theme_term = pkgs.writeShellScriptBin "theme_term" ''
+    #!/usr/bin/env bash
+    WIN_PATH_RAW=$(reg.exe query "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper | grep Wallpaper | awk '{print $3}')
+    WSL_PATH=$(echo "$WIN_PATH_RAW" | tr -d '\r' | sed 's|\\|/|g' | sed 's|C:|/mnt/c|')
+    if [ -f "$WSL_PATH" ]; then
+      wal -i "$WSL_PATH" -q -n
+    else
+      echo "Wallpaper path not found or is invalid: $WSL_PATH"
+    fi
+  '';
 in
 {
   options.nyx.modules.shell.wal = {
@@ -12,6 +22,9 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       pywal
+      theme_term
     ];
   };
 }
+
+
