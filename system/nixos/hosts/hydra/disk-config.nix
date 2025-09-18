2770745fc0800c8fec49578e9f591fae7b05f7d0
Mike D. <66962344+mwdavisii@users.noreply.github.com>
2025-09-18 07:59:49 -0500

    Homelab (#67)

---
+++ system/nixos/hosts/hydra/disk-config.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/root";
+      fsType = "ext4";
+    };
+
+  swapDevices =
+    [
+      { 
+        device = "/dev/disk/by-label/swap"; 
+      }
+    ];
+} 
