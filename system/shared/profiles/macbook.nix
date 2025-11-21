6dede427744a1abba5644a75e7e570794720e184
Mike D. <mwdavisii@gmail.com>
2025-11-21 11:13:12 -0600

    adding ai cli and flake updates

--- system/shared/profiles/macbook.nix
+++ system/shared/profiles/macbook.nix
@@ -17,7 +17,7 @@ in
       packages = with pkgs; [
         noto-fonts
         noto-fonts-cjk-sans
-        noto-fonts-emoji
+        noto-fonts-color-emoji
         twemoji-color-font
         fira-code
         fira-code-symbols
