f0d889fde8e7daf06f0ab9a4b26e845ea01bae9d
Mike D. <mwdavisii@gmail.com>
2026-02-15 12:05:57 -0600

    hephaestus

--- system/nixos/hosts/hephaestus/disk-config.nix
+++ system/nixos/hosts/hephaestus/disk-config.nix
@@ -1,18 +1,19 @@
 { config, lib, pkgs, modulesPath, inputs, ... }:
 {
   fileSystems."/" ={ 
-      device = "/dev/disk/by-label/root";
+      device = "/dev/disk/by-uuid/a465158e-20e5-45f4-b6f0-693dea33e650";
       fsType = "ext4";
   };
 
   fileSystems."/boot" =  { 
-      device = "/dev/disk/by-label/BOOT";
+      device = "/dev/disk/by-uuid/5FC2-A833";
       fsType = "vfat";
   };
 
   swapDevices =[ 
     { 
-      device = "/dev/disk/by-label/swap"; 
+      device = "/dev/disk/by-uuid/9e377beb-b0fb-4aa0-aacd-6cbf61f4e89a"; 
     }
   ];
 }
+
--- system/nixos/hosts/hephaestus/home.nix
+++ system/nixos/hosts/hephaestus/home.nix
@@ -12,7 +12,7 @@
     sessionVariables = {
       PATH = "$PATH:~/.local/bin:~/.config/rofi/scripts";
     };
-    stateVersion = "25.05";
+    stateVersion = "26.05";
   };
 
   nyx = {
