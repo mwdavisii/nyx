c9f3f9f933c29350903eefd5c7a4f04eb3b6e553
Mike D. <mwdavisii@gmail.com>
2026-03-20 12:33:18 -0500

    adding jellyfin client

--- setup/arch/02-install-packages.sh
+++ setup/arch/02-install-packages.sh
@@ -275,7 +275,8 @@ yay -S --needed --noconfirm \
   obsidian \
   claude-desktop-bin \
   sublime-text-4 \
-  pear-desktop
+  pear-desktop \
+  jellyfin-desktop
 
 info "Installing Hyprland plugins via hyprpm..."
 # Use system cmake/pkg-config, not Nix's, which can't find system packages
