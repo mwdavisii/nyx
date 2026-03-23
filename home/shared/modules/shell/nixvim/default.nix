e988d0d2600456e38dfc3507f16ebbbd3a3a3f76
Mike D. <mwdavisii@gmail.com>
2026-03-23 09:46:08 -0500

    added wl-copy for arch build compatability with neovim

--- home/shared/modules/shell/nixvim/default.nix
+++ home/shared/modules/shell/nixvim/default.nix
@@ -37,7 +37,6 @@ in
   config = mkIf cfg.enable {
     programs.nixvim = {
       enable = true;
-      extraPackages = with pkgs; [ wl-clipboard ];
 
       # basic editor feel
       opts = {
@@ -111,9 +110,10 @@ in
 
       # ===== Tools Telescope uses under the hood (+ TS toolchain/parsers)
       extraPackages = with pkgs; [
-        ripgrep  # live_grep
-        fd       # file finder
+        ripgrep      # live_grep
+        fd           # file finder
         tree-sitter
+        wl-clipboard # system clipboard on Wayland
       ] ++ treeSitterGrammars;
 
       # ===== Keymaps: tree + telescope
